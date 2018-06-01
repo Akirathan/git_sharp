52f4c386f3331f9516ae37f20eee5c2f89f7517e
mayfa <pavel.akira.marek@gmail.com>
2018-06-01 11:50:15 +0200

    HashKey: Add GetStringRepresentation

--- GitSharp/hash/HashKey.cs
+++ GitSharp/hash/HashKey.cs
@@ -1,4 +1,6 @@
-﻿namespace GitSharp.hash {
+﻿using System.Text;
+
+namespace GitSharp.Hash {
 	public class HashKey {
 		private byte[] content;
 		
@@ -9,6 +11,25 @@
 
 		public byte[] Content {
 			get { return content; }
+		}
+
+		/// <summary>
+		/// Returns hexadecimal string representation (of length 40) of this HashKey.
+		/// Note that Base64 encoding is not used due to possible conflicts.
+		/// </summary>
+		/// <returns> string representing hexadecimal format of this HashKey </returns>
+		public string GetStringRepresentation()
+		{
+			return GetHexRepresentation(content);
+		}
+
+		private string GetHexRepresentation(byte[] bytes)
+		{
+			StringBuilder sb = new StringBuilder(bytes.Length * 2);
+			foreach (byte b in bytes) {
+				sb.AppendFormat("{0:x2}", b);
+			}
+			return sb.ToString();
 		}
 	}
 }
