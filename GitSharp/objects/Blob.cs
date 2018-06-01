aab94d6d773e4291feb3d8b3242b1fbd43e3de98
mayfa <pavel.akira.marek@gmail.com>
2018-06-01 12:42:34 +0200

    Blob: add Content

--- GitSharp/objects/Blob.cs
+++ GitSharp/objects/Blob.cs
@@ -1,5 +1,14 @@
 ﻿namespace GitSharp.Objects {
 	internal class Blob : GitObject {
+		private string content;
 		
+		public Blob(string content)
+		{
+			this.content = content;
+		}
+
+		public string Content {
+			get { return content; }
+		}
 	}
 }
