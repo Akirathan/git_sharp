ff4e771a7a359b9fcd2b2114a9cc10900555a610
mayfa <pavel.akira.marek@gmail.com>
2018-06-02 11:17:28 +0200

    Blob: add parsing from file content and generating file content functionality

--- GitSharp/objects/Blob.cs
+++ GitSharp/objects/Blob.cs
@@ -1,6 +1,27 @@
-﻿namespace GitSharp.Objects {
+﻿using System.IO;
+using System.Text;
+
+namespace GitSharp.Objects {
 	internal class Blob : GitObject {
+		private const string BlobFileType = "blob";
 		private string content;
+
+		public static Blob ParseFromString(string content)
+		{
+			StringReader reader = new StringReader(content);
+			if (reader.ReadLine() != BlobFileType) {
+				return null;
+			}
+			return new Blob(reader.ReadToEnd());
+		}
+
+		public static string CreateBlobFileContent(Blob blob)
+		{
+			StringBuilder contentBuilder = new StringBuilder();
+			contentBuilder.AppendLine(BlobFileType);
+			contentBuilder.Append(blob.Content);
+			return contentBuilder.ToString();
+		}
 		
 		public Blob(string content)
 		{
