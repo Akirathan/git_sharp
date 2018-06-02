b6c2b669df4f9d60a5f1253e5580e944f8b2cb3c
mayfa <pavel.akira.marek@gmail.com>
2018-06-02 13:09:17 +0200

    Blob: implement IEquatable<Blob>

--- GitSharp/objects/Blob.cs
+++ GitSharp/objects/Blob.cs
@@ -1,8 +1,9 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Text;
 
 namespace GitSharp.Objects {
-	internal class Blob : GitObject {
+	internal class Blob : GitObject, IEquatable<Blob> {
 		private const string BlobFileType = "blob";
 		private string content;
 
@@ -30,6 +31,26 @@ namespace GitSharp.Objects {
 
 		public string Content {
 			get { return content; }
+		}
+
+		public bool Equals(Blob other)
+		{
+			if (ReferenceEquals(null, other)) return false;
+			if (ReferenceEquals(this, other)) return true;
+			return string.Equals(content, other.content);
+		}
+
+		public override bool Equals(object obj)
+		{
+			if (ReferenceEquals(null, obj)) return false;
+			if (ReferenceEquals(this, obj)) return true;
+			if (obj.GetType() != typeof(Blob)) return false;
+			return Equals((Blob) obj);
+		}
+
+		public override int GetHashCode()
+		{
+			return content.GetHashCode();
 		}
 	}
 }
