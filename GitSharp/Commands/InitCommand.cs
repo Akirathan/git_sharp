e9fae4e6a4ba1f19711384cf669093946931548f
mayfa <pavel.akira.marek@gmail.com>
2018-06-07 18:16:22 +0200

    InitCommand: add some implementation

--- GitSharp/Commands/InitCommand.cs
+++ GitSharp/Commands/InitCommand.cs
@@ -1,8 +1,55 @@
-﻿namespace GitSharp.Commands {
+﻿using System;
+using System.IO;
+
+namespace GitSharp.Commands {
 	internal class InitCommand : Command {
 		public override void Process()
 		{
-			throw new System.NotImplementedException();
+			string rootDirPath = Traverser.GetRootDirPath();
+			if (rootDirPath != null) {
+				PrintTryingToReinitializeRepo(rootDirPath);
+				return;
+			}
+			
+			CreateRootDir();
+			CreateAllFilesDirectories();
+		}
+
+		private void PrintTryingToReinitializeRepo(string rootPath)
+		{
+			Console.WriteLine($"There is already git direcotry in {rootPath}");
+			Console.WriteLine("  If you want to reinitialize, remove it first");
+		}
+
+		private void CreateRootDir()
+		{
+			CreateDirectory(Traverser.GitRootDirName);
+		}
+		
+		private void CreateAllFilesDirectories()
+		{
+			CreateDirectory(ObjectDatabase.DefaultPath);
+			CreateFile();
+		}
+
+		private void CreateDirectory(string dirName)
+		{
+			try {
+				Directory.CreateDirectory(dirName);
+			}
+			catch (Exception e) {
+				throw new Exception($"Cannot create directory {dirName}", e);
+			}
+		}
+
+		private void CreateFile(string fileName)
+		{
+			try {
+				System.IO.File.Create(fileName);
+			}
+			catch (Exception e) {
+				throw new Exception($"Cannot create file {fileName}", e);
+			}
 		}
 	}
 }
