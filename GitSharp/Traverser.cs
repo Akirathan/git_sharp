8a6b29221d03c1d040e2ad8201743f29c703fada
mayfa <pavel.akira.marek@gmail.com>
2018-06-07 10:07:30 +0200

    Traverser: implement GetRootDirPath

--- GitSharp/Traverser.cs
+++ GitSharp/Traverser.cs
@@ -1,8 +1,51 @@
-﻿namespace GitSharp {
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace GitSharp {
 	public static class Traverser {
-		public static void UpdateWorkingDirectoryIndex()
+		private const string GitRootDirName = ".git_sharp";
+		
+		private static string _rootDirPath;
+
+		/// <summary>
+		/// Returns absolute path to the directory, where .git is stored.
+		/// Note that this method allows git to be invokend in some subdirectory.
+		/// </summary>
+		/// <returns>
+		/// null when there is no .git in all parents.
+		/// </returns>
+		public static string GetRootDirPath()
 		{
-			
+			if (_rootDirPath != null) {
+				return _rootDirPath;
+			}
+
+			string enumeratedDir = Directory.GetCurrentDirectory();
+			while (!DirContainsGitDir(enumeratedDir)) {
+				DirectoryInfo currDirInfo = Directory.GetParent(enumeratedDir);
+				if (currDirInfo == null) {
+					return null;
+				}
+				enumeratedDir = currDirInfo.Name;
+			}
+			_rootDirPath = enumeratedDir;
+			return enumeratedDir;
+		}
+		
+		public static IEnumerable<string> GetAllFiles()
+		{
+        	return Directory.EnumerateFiles(Directory.GetCurrentDirectory(), "*", SearchOption.AllDirectories);
+		}
+
+		private static bool DirContainsGitDir(string dirPath)
+		{
+			IEnumerable<string> directories = Directory.EnumerateDirectories(dirPath);
+			foreach (string directory in directories) {
+				if (directory == GitRootDirName) {
+					return true;
+				}
+			}
+			return false;
 		}
 	}
 }
