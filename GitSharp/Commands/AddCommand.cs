bd14699a3470d378a6af3f7e10ab91e289e18fda
mayfa <pavel.akira.marek@gmail.com>
2018-06-07 19:25:07 +0200

    AddCommand: add string[] args to constructor

--- GitSharp/Commands/AddCommand.cs
+++ GitSharp/Commands/AddCommand.cs
@@ -1,5 +1,12 @@
 ﻿namespace GitSharp.Commands {
 	internal class AddCommand : Command {
+		private string[] _args;
+		
+		public AddCommand(string[] args)
+		{
+			_args = args;
+		}
+		
 		public override void Process()
 		{
 			throw new System.NotImplementedException();
