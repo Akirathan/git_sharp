7a67a04b27add56c13136f2df2cb9d3b4e310b3f
mayfa <pavel.akira.marek@gmail.com>
2018-06-07 19:24:38 +0200

    Cli: add simple parsing functionality

--- GitSharp/Cli.cs
+++ GitSharp/Cli.cs
@@ -1,10 +1,25 @@
-﻿using GitSharp.Commands;
+﻿using System;
+using GitSharp.Commands;
 
 namespace GitSharp {
 	internal static class Cli {
-		public static Command ParseCommand(string cmdLine)
+		private const string InitCommandName = "init";
+		private const string StatusCommandName = "status";
+		private const string AddCommandName = "add";
+		
+		public static Command ParseCommand(string[] args)
 		{
-			return null;
+			if (args[0] == InitCommandName) {
+				return new InitCommand();
+			}
+			if (args[0] == StatusCommandName) {
+				return new StatusCommand();
+			}
+			if (args[0] == AddCommandName) {
+				string[] restOfArgs = new string[args.Length - 1];
+				Array.Copy(args, 1, restOfArgs, 0, restOfArgs.Length);
+				return new AddCommand(restOfArgs);
+			}
 		}
 	}
 }
