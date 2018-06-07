af609177606353bbfbd197821a6baa0a05c733be
mayfa <pavel.akira.marek@gmail.com>
2018-06-07 17:27:29 +0200

    ContentHasher: add HashFileContent method

--- GitSharp/Hash/ContentHasher.cs
+++ GitSharp/Hash/ContentHasher.cs
@@ -1,4 +1,6 @@
-﻿using System.Security.Cryptography;
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
 
 namespace GitSharp.Hash {
 	public class ContentHasher {
@@ -10,6 +12,26 @@ namespace GitSharp.Hash {
             SHA1 sha = SHA1.Create();
             byte[] hash = sha.ComputeHash(bytes);
 			return new HashKey(hash);
+		}
+
+		public static HashKey hashFileContent(string fileName)
+		{
+			string content;
+			StreamReader reader = null;
+			try {
+				reader = new StreamReader(fileName);
+				content = reader.ReadToEnd();
+			}
+			catch (IOException e) {
+				throw new Exception($"Error when reading file {fileName}", e);
+			}
+			finally {
+				if (reader != null) {
+					reader.Close();
+				}
+			}
+
+			return hash(content);
 		}
 	}
 }
