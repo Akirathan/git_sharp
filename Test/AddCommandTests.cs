8cf362db6acd1553da6820cdf7e041474733f3f9
mayfa <pavel.akira.marek@gmail.com>
2018-06-08 16:11:01 +0200

    Fix unit tests project

---
+++ Test/AddCommandTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using Xunit;
+using GitSharp.Commands;
+
+namespace Test {
+	public class AddCommandTests {
+		[Fact]
+		public void Test1()
+		{
+			CreateFile("a.txt", "Nazdar");
+			AddCommand addCommand = new AddCommand(new string[] {"a.txt"});
+		}
+
+		private void CreateFile(string fileName, string content)
+		{
+			FileStream fileStream = File.Create(fileName);
+			using (StreamWriter writer = new StreamWriter(fileStream)) {
+				writer.Write(content);
+			}
+		}
+	}
+}
