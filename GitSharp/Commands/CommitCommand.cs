7e28b0c14f6ad276a82b94824b28e50128dc6062
mayfa <pavel.akira.marek@gmail.com>
2018-06-09 15:34:57 +0200

    Add CommitCommand

---
+++ GitSharp/Commands/CommitCommand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GitSharp.Commands {
+	internal class CommitCommand : Command {
+		private string[] _args;
+		private string _message;
+		
+		public CommitCommand(string[] args)
+		{
+			_args = args;
+		}
+		
+		public override void Process()
+		{
+			if (!ProcessOptions(_args)) {
+				PrintHelp();
+				return;
+			}
+		}
+
+		private void PrintHelp()
+		{
+			Console.WriteLine("usage: \"git commit <message>\"");
+		}
+		
+		private bool ProcessOptions(string[] args)
+		{
+			if (args.Length != 1) {
+				return false;
+			}
+			_message = args[0];
+			return true;
+		}
+	}
+}
