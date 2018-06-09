72ef191955069421477059d00225ada232470f55
mayfa <pavel.akira.marek@gmail.com>
2018-06-09 18:42:52 +0200

    Add TreeBuilder class

---
+++ GitSharp/Objects/TreeBuilder.cs
@@ -0,0 +1,22 @@
+﻿namespace GitSharp.Objects {
+	/// <summary>
+	/// Is not immutable like Tree.
+	/// Particular useful for creating tree objects
+	/// </summary>
+	internal class TreeBuilder {
+		public Tree CreateImmutableTree()
+		{
+			return null;
+		}
+		
+		public TreeBuilder FindOrCreateSubTree(string[] dirNames)
+		{
+			return null;
+		}
+
+		public void AddBlob(Blob blob)
+		{
+			
+		}
+	}
+}
