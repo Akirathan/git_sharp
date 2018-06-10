1f641aa71954959c246fed52a58a849e2fd1c875
mayfa <pavel.akira.marek@gmail.com>
2018-06-10 19:15:18 +0200

    Add TreePrinter (that will be common for printing Tree and TreeBuilder)

---
+++ GitSharp/Objects/TreePrinter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using GitSharp.Hash;
+
+namespace GitSharp.Objects {
+	internal static class TreePrinter {
+		private const string TreeFileType = "tree";
+		
+		public static string PrintToString(string dirName, IDictionary<string, HashKey> blobs,
+			IDictionary<string, HashKey> subTrees)
+		{
+			StringBuilder contentBuilder = new StringBuilder();
+			
+			contentBuilder.AppendLine(TreeFileType + " " + dirName);
+			
+            foreach (KeyValuePair<string, HashKey> pair in blobs) {
+	            HashKey key = pair.Value;
+	            string fileName = pair.Key;
+                contentBuilder.AppendLine("blob " + key.ToString() + " " + fileName);
+            }
+			
+            foreach (KeyValuePair<string, HashKey> pair in subTrees) {
+	            HashKey key = pair.Value;
+	            string subTreeDirName = pair.Key;
+                contentBuilder.AppendLine("tree " + key.ToString() + " " + subTreeDirName);
+            }
+
+			return contentBuilder.ToString();
+		}
+	}
+}
