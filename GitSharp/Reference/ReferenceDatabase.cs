0161a1c45e4cf1bb812b46c7d7d859b573362b62
mayfa <pavel.akira.marek@gmail.com>
2018-06-10 18:07:36 +0200

    ReferenceDatabase: implement SetHead and GetHead, also add some private members

--- GitSharp/Reference/ReferenceDatabase.cs
+++ GitSharp/Reference/ReferenceDatabase.cs
@@ -1,35 +1,69 @@
 ﻿using System.Collections.Generic;
+using System.IO;
 using GitSharp.Hash;
 
 namespace GitSharp.Reference {
 	internal static class ReferenceDatabase {
-		private static readonly IDictionary<string, Branch> _branches = new Dictionary<string, Branch>();
+		private static readonly string HeadFilePath =
+			Traverser.GitRootDirName + Path.DirectorySeparatorChar + "HEAD";
+
+		private static readonly string RefsDirPath =
+			Traverser.GitRootDirName + Path.DirectorySeparatorChar + "refs";
+
+		private static readonly string BranchDirPath =
+			RefsDirPath + Path.DirectorySeparatorChar + "heads";
 		
-		static ReferenceDatabase()
+		private static readonly IDictionary<string, Branch> _trackedBranches = new Dictionary<string, Branch>();
+
+		private static Branch _head;
+
+		/// <summary>
+		/// Initializes whole directory structure necessary for this ReferenceDatabase
+		/// to work.
+		/// Called once from "git init".
+		/// </summary>
+		public static void Init()
 		{
-			
+			Directory.CreateDirectory(BranchDirPath);
+			InitHeadFile();
 		}
 
 		/// <summary>
 		/// Saves all modified (or created) branches.
+		/// Also saves HEAD.
 		/// </summary>
 		public static void Dispose()
 		{
-			foreach (Branch branch in _branches.Values) {
+			foreach (Branch branch in _trackedBranches.Values) {
 				if (branch.IsModified) {
 					// TODO: save branch.
 				}
 			}
+
+			if (_head != null && _head.IsModified) {
+				SaveBranch(_head);
+			}
 		}
 
+		/// <summary>
+		/// Returns current head branch
+		/// </summary>
+		/// <returns></returns>
 		public static Branch GetHead()
 		{
-			
+			string headContent = ReadFile(HeadFilePath);
+			if (headContent == "0" && _head == null) {
+                _head = CreateBranch("master", null);
+			}
+			else if (_head == null) {
+				_head = GetBranch(headContent);
+			}
+			return _head;
 		}
 
 		public static void SetHead(Branch branch)
 		{
-			
+			_head = branch;
 		}
 
 		public static Branch GetBranch(string branchName)
@@ -45,8 +79,35 @@ namespace GitSharp.Reference {
 		public static Branch CreateBranch(string name, HashKey commitKey)
 		{
 			Branch branch = new Branch(name, commitKey);
-			_branches.Add(name, branch);
+			_trackedBranches.Add(name, branch);
 			return branch;
 		}
+
+		private static void InitHeadFile()
+		{
+			FileStream fileStream = System.IO.File.Create(HeadFilePath);
+			using (StreamWriter writer = new StreamWriter(fileStream)) {
+				writer.Write("0");
+			}
+		}
+		
+		private static void SaveBranch(Branch branch)
+		{
+			using (StreamWriter writer =
+				new StreamWriter(BranchDirPath + Path.DirectorySeparatorChar + branch.Name))
+			{
+				writer.Write(branch.GetCommitKey().ToString());
+			}
+		}
+
+		private static string ReadFile(string fileName)
+		{
+			string content;
+			using (StreamReader reader = new StreamReader(fileName)) {
+				content = reader.ReadToEnd();
+			}
+			return content;
+		}
+
 	}
 }
