f72df37616395b59685a9f001e690d4b01bd932e
mayfa <pavel.akira.marek@gmail.com>
2018-06-10 16:30:58 +0200

    Add ReferenceDatabase API

---
+++ GitSharp/Reference/ReferenceDatabase.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using GitSharp.Hash;
+
+namespace GitSharp.Reference {
+	internal static class ReferenceDatabase {
+		private static IDictionary<string, Branch> _branches;
+		
+		static ReferenceDatabase()
+		{
+			
+		}
+
+		public static void Dispose()
+		{
+			
+		}
+
+		public static Branch GetHead()
+		{
+			
+		}
+
+		public static void SetHead(Branch branch)
+		{
+			
+		}
+
+		public static Branch GetBranch(string branchName)
+		{
+			
+		}
+		
+		public static IEnumerable<Branch> GetAllBranches()
+		{
+			
+		}
+
+		public static Branch CreateBranch(string name, HashKey commitKey)
+		{
+			
+		}
+	}
+}
