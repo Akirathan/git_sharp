f0af1888512fd53318176b482dc8a267f95e267a
mayfa <pavel.akira.marek@gmail.com>
2018-06-10 16:31:59 +0200

    Add Branch and its API

---
+++ GitSharp/Reference/Branch.cs
@@ -0,0 +1,25 @@
+﻿using GitSharp.Hash;
+using GitSharp.Objects;
+
+namespace GitSharp.Reference {
+	internal class Branch {
+		public static Branch ParseFromString(string content)
+		{
+			return null;
+		}
+
+		public HashKey CommitKey { get; }
+		public string Name { get; }
+		private Commit _commit;
+
+		public Branch(string name, HashKey commitKey)
+		{
+			
+		}
+
+		public Commit LoadCommit()
+		{
+			return null;
+		}
+	}
+}
