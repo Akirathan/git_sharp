06547e223fa368655b24939e082b86c8371c0026
mayfa <pavel.akira.marek@gmail.com>
2018-06-11 13:42:01 +0200

    Add RelativePath class

---
+++ GitSharp/RelativePath.cs
@@ -0,0 +1,24 @@
+﻿namespace GitSharp {
+	internal class RelativePath {
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="path">
+		/// arbitrary path - absolute, relative to anything, ...
+		/// </param>
+		public RelativePath(string path)
+		{
+			
+		}
+
+		string GetRelativeToGitRoot()
+		{
+			string gitRootAbsolute = Traverser.GetRootDirPath();
+		}
+
+		string GetAbsolutePath()
+		{
+			return null;
+		}
+	}
+}
