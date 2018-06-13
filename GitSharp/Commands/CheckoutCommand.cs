b86ac4689a0c36c0ef7b809840274af159de5499
mayfa <pavel.akira.marek@gmail.com>
2018-06-13 18:20:24 +0200

    Add CheckoutCommand

---
+++ GitSharp/Commands/CheckoutCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GitSharp.Commands {
+	internal class CheckoutCommand : Command {
+		public override void Process()
+		{
+			throw new System.NotImplementedException();
+		}
+
+		private void PrintHelp()
+		{
+			Console.WriteLine("usage: \"checkout <branch>\"");
+		}
+	}
+}
