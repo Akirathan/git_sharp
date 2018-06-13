4169e91131aa7686576a39e48ec0344c1dd6d135
mayfa <pavel.akira.marek@gmail.com>
2018-06-13 18:21:53 +0200

    Add BranchCommand

---
+++ GitSharp/Commands/BranchCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GitSharp.Commands {
+	internal class BranchCommand : Command {
+		public BranchCommand(string[] args)
+		{
+			
+		}
+		
+		public override void Process()
+		{
+			throw new System.NotImplementedException();
+		}
+
+		private void PrintHelp()
+		{
+			Console.WriteLine("usage: \"branch <name>\"");
+		}
+	}
+}
