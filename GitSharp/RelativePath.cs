fba03a96340c8a271acb87df3b5a6d26335f59ae
mayfa <pavel.akira.marek@gmail.com>
2018-06-13 10:55:36 +0200

    RelativePath: fix _absolutePath creation

--- GitSharp/RelativePath.cs
+++ GitSharp/RelativePath.cs
@@ -1,7 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Runtime.CompilerServices;
+using System.Text;
 
 namespace GitSharp {
+	/// <summary>
+	/// It is not used to control user input - this is responsibility of Commands.
+	/// It is used for purposes of ObjectDatabase and Index ie. for converting
+	/// to absolute path so file operations can be done.
+	/// </summary>
 	internal class RelativePath : IEquatable<RelativePath> {
 		private string _path;
 		private string _absolutePath;
@@ -21,7 +29,13 @@ namespace GitSharp {
 				_absolutePath = _path;
 			}
 			else {
-				_absolutePath = Directory.GetCurrentDirectory() + Path.DirectorySeparatorChar + _path;
+				string cwd = Directory.GetCurrentDirectory();
+				if (PathsIntersects(cwd, _path)) {
+					_absolutePath = IntersectedPathsJoin(cwd, _path);
+				}
+				else {
+                    _absolutePath = cwd + Path.DirectorySeparatorChar + _path;
+				}
 			}
 			
 			string gitRootAbsolute = Traverser.GetRootDirPath();
@@ -56,6 +70,55 @@ namespace GitSharp {
 		public override int GetHashCode()
 		{
 			return _path.GetHashCode();
+		}
+
+		private bool PathsIntersects(string pathA, string pathB)
+		{
+			string[] pathAItems =
+				pathA.Split(new char[] {Path.DirectorySeparatorChar}, StringSplitOptions.RemoveEmptyEntries);
+			
+			string[] pathBItems =
+				pathB.Split(new char[] {Path.DirectorySeparatorChar}, StringSplitOptions.RemoveEmptyEntries);
+
+			for (int i = 0; i < pathAItems.Length; i++) {
+				if (pathAItems[i] == pathBItems[0]) {
+					return true;
+				}
+			}
+			return false;
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="pathA">absolute path</param>
+		/// <param name="pathB">relative path that intersects with pathA</param>
+		/// <returns>joined absolute path</returns>
+		private string IntersectedPathsJoin(string pathA, string pathB)
+		{
+			string[] pathAItems =
+				pathA.Split(new char[] {Path.DirectorySeparatorChar}, StringSplitOptions.RemoveEmptyEntries);
+			
+			string[] pathBItems =
+				pathB.Split(new char[] {Path.DirectorySeparatorChar}, StringSplitOptions.RemoveEmptyEntries);
+
+			StringBuilder builder = new StringBuilder();
+			builder.Append(Path.DirectorySeparatorChar);
+			
+			for (int i = 0; pathAItems[i] != pathBItems[0]; i++) {
+				builder.Append(pathAItems[i]);
+				builder.Append(Path.DirectorySeparatorChar);
+			}
+
+			foreach (string pathBItem in pathBItems) {
+				builder.Append(pathBItem);
+				builder.Append(Path.DirectorySeparatorChar);
+			}
+
+			// Remove last DirectorySeparatorChar
+			builder.Remove(builder.Length - 1, 1);
+
+			return builder.ToString();
 		}
 	}
 }
