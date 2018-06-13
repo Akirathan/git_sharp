36c059cda45f936c2d1fbe4959a0ba255532a2c5
mayfa <pavel.akira.marek@gmail.com>
2018-06-13 18:09:21 +0200

    Implement LogCommand

--- GitSharp/Commands/LogCommand.cs
+++ GitSharp/Commands/LogCommand.cs
@@ -1,8 +1,39 @@
-﻿namespace GitSharp.Commands {
+﻿using System;
+using System.Collections.Generic;
+using GitSharp.Objects;
+using GitSharp.Reference;
+
+namespace GitSharp.Commands {
 	internal class LogCommand : Command {
 		public override void Process()
 		{
-			throw new System.NotImplementedException();
+			Branch headBranch = ReferenceDatabase.GetHead();
+
+			for (Commit commit = headBranch.LoadCommit(); commit != null; commit = commit.LoadParent()) {
+				PrintCommit(commit);
+			}
+		}
+
+		private void PrintCommit(Commit commit)
+		{
+			Console.WriteLine("==============================");
+			Console.WriteLine("commit " + commit.GetChecksum().ToString());
+			Console.WriteLine();
+			Console.WriteLine(commit.Message);
+			Console.WriteLine();
+			Console.WriteLine("Modified files:");
+			PrintAllModifiedFiles(commit);
+			Console.WriteLine("==============================");
+		}
+
+		private void PrintAllModifiedFiles(Commit commit)
+		{
+			Tree tree = commit.LoadTree();
+			List<Blob> blobs = new List<Blob>();
+			tree.LoadAndGetAllBlobs(blobs);
+			foreach (Blob blob in blobs) {
+				Console.WriteLine($"  ${blob.FilePath}");
+			}
 		}
 	}
 }
