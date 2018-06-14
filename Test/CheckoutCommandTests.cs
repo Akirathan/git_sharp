728743b88332cf66a24182f2a831a1e8a59c8cca
mayfa <pavel.akira.marek@gmail.com>
2018-06-14 17:59:38 +0200

    Unit tests: add CheckoutCommandTests

---
+++ Test/CheckoutCommandTests.cs
@@ -0,0 +1,48 @@
+﻿using System.IO;
+using GitSharp;
+using GitSharp.Commands;
+using GitSharp.Reference;
+using GitSharp.Objects;
+using Xunit;
+
+namespace Test {
+	public class CheckoutCommandTests {
+		
+		[Fact]
+		public void TwoCommitsCheckoutToFirstCommitTest()
+		{
+			Init();
+			
+			CreateFile("a.txt", "a content");
+			Program.Main(new string[]{"add", "a.txt"});
+			Program.Main(new string[]{"commit", "First commit"});
+			Program.Main(new string[]{"branch", "branch-1"});
+			
+			CreateFile("a.txt", "modified content");
+			Program.Main(new string[]{"add", "a.txt"});
+			Program.Main(new string[]{"commit", "Second commit"});
+			Program.Main(new string[]{"branch", "branch-2"});
+			
+			Program.Main(new string[]{"checkout", "branch-1"});
+
+			Tree headTree = ReferenceDatabase.GetHead().LoadCommit().LoadTree();
+			Blob aFileBlob = headTree.FindAndLoadBlob("a.txt");
+			Assert.Equal(aFileBlob.FileContent, "a content");
+		}
+		
+		private void Init()
+		{
+			if (Directory.Exists(".git_sharp")) {
+                Directory.Delete(".git_sharp", recursive: true);
+			}
+			new InitCommand().Process();
+		}
+		
+		private void CreateFile(string fileName, string content)
+		{
+			using (StreamWriter writer = new StreamWriter(fileName)) {
+				writer.Write(content);
+			}
+		}
+	}
+}
