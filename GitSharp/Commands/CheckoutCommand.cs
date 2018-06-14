b34afb6794b8b803340b8dc8e097d187b8e1a347
mayfa <pavel.akira.marek@gmail.com>
2018-06-14 15:31:24 +0200

    implement CheckoutCommand

--- GitSharp/Commands/CheckoutCommand.cs
+++ GitSharp/Commands/CheckoutCommand.cs
@@ -1,15 +1,60 @@
 ﻿using System;
+using GitSharp.Reference;
 
 namespace GitSharp.Commands {
 	internal class CheckoutCommand : Command {
+		private string[] _args;
+		private Branch _branch;
+		
+		public CheckoutCommand(string[] args)
+		{
+			_args = args;
+		}
+		
 		public override void Process()
 		{
-			throw new System.NotImplementedException();
+			if (!ProcessArgs()) {
+				return;
+			}
+
+			if (!_branch.LoadCommit().Checkout()) {
+				PrintError();
+				return;
+			}
+			
+			ReferenceDatabase.SetHead(_branch);
+			PrintSuccess();
 		}
 
 		private void PrintHelp()
 		{
 			Console.WriteLine("usage: \"checkout <branch>\"");
+		}
+
+		private void PrintError()
+		{
+			Console.WriteLine("Some of local changes would be modified by the checkout, exiting ...");
+		}
+
+		private void PrintSuccess()
+		{
+			Console.WriteLine($"{_branch.Name} checked out");
+		}
+
+		private bool ProcessArgs()
+		{
+			if (_args.Length != 1) {
+				PrintHelp();
+				return false;
+			}
+
+			_branch = ReferenceDatabase.GetBranch(_args[0]);
+			if (_branch == null) {
+				Console.WriteLine($"{_args[0]} does not specify any branch name");
+				return false;
+			}
+
+			return true;
 		}
 	}
 }
