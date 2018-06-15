b79996ea707de1d81e6ab0850b75cf3907dbb88a
mayfa <pavel.akira.marek@gmail.com>
2018-06-15 17:37:24 +0200

    IGitObject: add comments

--- GitSharp/Objects/IGitObject.cs
+++ GitSharp/Objects/IGitObject.cs
@@ -2,9 +2,13 @@
 
 namespace GitSharp.Objects {
 	/// <summary>
-	/// Represents git object that can be retrieved from ObjectDatabase
+	/// Represents git object that can be retrieved from <see cref="ObjectDatabase"/>.
 	/// </summary>
 	internal interface IGitObject {
+		/// <summary>
+		/// Returns a key that is used for retrieving this object from <see cref="ObjectDatabase"/>.
+		/// </summary>
+		/// <returns></returns>
 		HashKey GetChecksum();
 	}
 }
